c2dd81b3ef2cbc504ccdf1498189aade328f66ca
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-11-07 10:14:02 +0900

    init

---
+++ lsp-addon-jump-to-error.el
@@ -0,0 +1,26 @@
+;;; lsp-addon-jump-to-error.el --- Minor mode for jump-to-error on save with lsp-mode -*- lexical-binding: t -*-
+
+;;; Copyright (C) 2018 Kobayasi, Hiroaki <buribullet@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Kobayasi, Hiroaki <buribullet@gmail.com>
+;; URL: https://github.com/hkoba/emacs-lsp-addon-jump-to-error
+;; Package-Requires: ((lsp-mode "5"))
+;; Version: 0.1
+
+;;; Commentary:
+
+;;; Code:
+
